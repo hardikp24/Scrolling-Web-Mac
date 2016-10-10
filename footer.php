c26d46ccd84919753ca1097cd838d4875d36388a
Hardik Patil <hardik.patil2494@gmail.com>
2016-10-10 14:03:03 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+	<p>HOME | ABOUT | SERVICES | CAREER | CONTACT @copyright2016 
+	</p>
+</footer>
---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Mac Web | Home</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="mycss.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  <?php include('header.php'); ?> 
+
+<!--Header starts here-->
+<div class="sect sectOne">
+	<div class="content">
+		<div class="content-inner">
+			<h1 id="homeHeading"><span style="color:orange;">Hello</span> <span style="color:green;">Mac</span> <span style="color:orange;">Web</span>
+			</h1>
+                <p>Change your Career. Change your Life.</p>
+                <a href="#about" class="btn btn-primary btn-lg page-scroll">Find Out More</a>
+		</div>
+	</div>
+</div>
+<!--Header ends here-->
+
+<!--first subsection here-->
+<div class="subSection">
+	<div class="subsection-content">
+		<h1>New York Story Tellers</h1>
+		<p>	At The Story Studio, you will learn the art of storytelling, as taught by some of the most
+			accomplished storytellers across the country. <br>We provide friendly, expert training to help you
+			turn the most meaningful events of your life into unforgettable narratives.</p>
+
+	</div>
+</div>
+<!--first subsection  ends here-->
+
+<div class="sect sectTwo"></div>
+
+<div class="subSection">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-4">
+				<h3>History</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="col-md-4">
+				<h3>Legacy</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br></p>
+			</div>
+			<div class="col-md-4">
+				<h3>Future</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="sect sectThree"></div>
+<!--contact us here-->
+<section class="contact">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Contact</h2>
+				<p>(+91)9158482555 | hardik.patil2494@gmail.com </p>
+				<h2>Let's Get In Touch!</h2>
+				<p>Give us a call or send us an email and we will get back to you as soon as possible!</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="sect sectFour">
+	<h1>Thank you for Visiting</h1>
+</div>
+ 
+
+<?php include('footer.php');?>
+
+<!--footer here-->
+
+<!-- footer ends here -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
