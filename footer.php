0e8a9ce56440ba817403292b5ac6273af33810b0
Hardik Patil <hardik.patil2494@gmail.com>
2016-10-11 19:21:03 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -8,10 +8,10 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-12">
-				<h2>Contact</h2>
-				<h4> <span style="color: green;">(+91)9158482555</span> | <span style="color:maroon;">hardik.patil2494@gmail.com</span> </h4>
+				<h1 class="text-warning">Contact</h1>
+				<h3 class="text-success">(+91)9158482555</h3><h3 class="text-danger"> hardik.patil2494@gmail.com</h3>
 				<h2>Let's Get In Touch!</h2>
-				<p>Give us a call or send us an email and we will get back to you as soon as possible!</p>
+				<h3 class="text-primary">Give us a call or send us an email and we will get back to you as soon as possible!</h3>
 			</div>
 		</div>
 	</div>
