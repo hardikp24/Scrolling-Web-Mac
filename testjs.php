a0898f35758118ea2fc5308ed4ac0dfdfda58c64
Hardik Patil <Hardikpatil@Hardiks-MacBook-Air.local>
2016-10-13 01:14:25 +0530

    js tested success bg change onclick and date-time on hover

--- testjs.php
+++ testjs.php
@@ -4,20 +4,38 @@
 	<title>Test Js</title>
 	<link rel="stylesheet" type="text/css" href="">
 </head>
-<body>
+<body id="body">
+
+
+
 <center>
 	<a href="index.php">Home</a>
 	<div class="header">
-		<h1>Testing JavaScript</h1>
-		<p>With Hardik Patil</p>
+
+		<h1 id="heading">Testing JS Scripts</h1> 
+
+		<p>With Hardik Patil<br> This is page only for testing... </p>
 	</div>
 
 <h2>Entry Form</h2>
 <form name="myform" action="#" method="POST" onsubmit="return validateForm()">
+
+	<h1 id="time"></h1><br> <!--Display date here -->
+	
 	<label>User Name:</label>
-	<input type="text" name="uname"><br>
+	<input type="text" name="uname"><br><br>
 	<label>Password:</label>
-	<input type="text" name="pass"><br>
+	<input type="password" name="pass"><br><br>
+
+	<label>Choose color to change Background</label>
+	<select name="background" id="background" onchange="changeBackground(this)">
+		<option value="orange">Orange</option>
+		<option value="green">Green</option>
+		<option value="blue">Blue</option>
+	</select><br><br>
+
+	<button onmouseover="showDate()">What Day is today ?</button><br><br>
+
 	<input type="submit" value="Login">
 </form>
 
@@ -27,6 +45,9 @@
 
 
 <script type="text/javascript">
+
+// form validation here 
+
 function validateForm(){
 	var uname = document.forms['myform']['uname'].value;
 	if (uname == null || uname == "") {
@@ -47,10 +68,24 @@ function validateForm(){
 		alert("Password must be at least 5 character");
 		return false;
 	}
-	
-
-
 }
+
+
+//change background color 
+
+function changeBackground(x){
+	console.log(x.value);
+
+	var body = document.getElementById('body');
+	body.style.backgroundColor = x.value;
+}
+
+//Show current Date and Time on mouse hover 
+function showDate(){
+	var time = document.getElementById('time');
+	time.innerHTML = Date();
+}
+
 </script>
 
 
