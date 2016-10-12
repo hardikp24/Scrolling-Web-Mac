bd6de0f73e25e225c7f95127458ad11b563768ac
Hardik Patil <Hardikpatil@Hardiks-MacBook-Air.local>
2016-10-12 16:59:22 +0530

    unnecessarycache

---
+++ testjs.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Test Js</title>
+
+
+	
+
+</head>
+<body>
+<script type="text/javascript">
+		console.log('Hello Chrome');
+	</script>
+
+
+</body>
+</html>
+
+
