cd550fc5280b0135294b850deba79cb177035bda
hardikp24 <hardik.patil2494@gmail.com>
2016-10-13 21:00:43 +0530

    solved styling issue occured between top navbar and below it's modules

--- register.php
+++ register.php
@@ -1,82 +1,87 @@
 <title>Register Here</title>
 <?php include('header.php'); ?>
 
-<!-- Login form initiate here-->
-<form class="login" action="" method=""> 
-	<h3>Already have an accont? Login Here</h3> <br>
-	<div class="row col-xs-10">
 
-		<div class="form-group">
-			Login ID: 	<input type="text" placeholder="Email" class="form-control">
-			Password: 	<input type="password" placeholder="Password" class="form-control">
+<div class="register_login">
 
-			<div class="checkbox">
-		    	<label><input type="checkbox"> Remember me</label>
-		  	</div>
+	<!-- Login form initiate here-->
+	<form class="login" action="" method=""> 
+		<h3>Already have an accont? Login Here</h3> <br>
+		<div class="row col-xs-10">
 
-			<button type="submit" class="btn btn-success">Sign in</button>
-		
-		</div>
-	
-	</div>
-		
-</form>
-<!-- Login form ends here-->
+			<div class="form-group">
+				Login ID: 	<input type="text" placeholder="Email" class="form-control">
+				Password: 	<input type="password" placeholder="Password" class="form-control">
 
-<!-- Registeration here -->	
-<form class="register" action="" method="POST">
-	<h2>Create New Avatar here</h2>	
-		<div class="form-group">
+				<div class="checkbox">
+			    	<label><input type="checkbox"> Remember me</label>
+			  	</div>
 
-			<div class="row">
-				<div class="col-xs-4">
-					First Name: <input type="text" name="fname" placeholder="Martin" class="form-control">
-				</div>	
-			</div>
+				<button type="submit" class="btn btn-success">Sign in</button>
 			
-			<div class="row">
-				<div class="col-xs-4">
-					Last Name: <input type="text" name="lname" placeholder="Peter" class="form-control">
-				</div>	
 			</div>
-			<div class="row">
-				<div class="col-xs-4">
-					Username  :<input type="text" name="uname" placeholder="MartinPeter12" class="form-control">
-				</div>	
-			</div>
-			<div class="row">
-						<div class="col-xs-6">
-							Email ID  :<input type="Email" name="emailid" placeholder="abc@example.com" class="form-control">
-						</div>	
-			</div>
-			<div class="row">
-						<div class="col-xs-4">
-						Password  :<input type="password" name="pass" placeholder="Password" class="form-control">
-						</div>	
-			</div>
-			<div class="row">
-						<div class="col-xs-4">
-							Confirmed Password:<input type="password" name="cpass" placeholder="Type same Password again" class="form-control">	
-						</div>	
-			</div>
-			<div class="row">
-						<div class="col-xs-3">
-							City      :<input type="text" name="city" placeholder="Los Angeles" class="form-control">
-						</div>	
-			</div><br>
-
-			<label class="radio-inline">
-				<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">Male
-			</label>
-			<label class="radio-inline">
-				<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">Female
-			</label>
-
+		
 		</div>
+			
+	</form>
+	<!-- Login form ends here-->
 
-	<button type="submit" class="btn btn-success">Register Here </button>
-</form><br><br>
+	<!-- Registeration here -->	
+	<form class="register" action="" method="POST">
+		<h2>Create New Avatar here</h2>	
+			<div class="form-group">
 
-<!-- Registeration ends here-->
+				<div class="row">
+					<div class="col-xs-4">
+						First Name: <input type="text" name="fname" placeholder="Martin" class="form-control">
+					</div>	
+				</div>
+				
+				<div class="row">
+					<div class="col-xs-4">
+						Last Name: <input type="text" name="lname" placeholder="Peter" class="form-control">
+					</div>	
+				</div>
+				<div class="row">
+					<div class="col-xs-4">
+						Username  :<input type="text" name="uname" placeholder="MartinPeter12" class="form-control">
+					</div>	
+				</div>
+				<div class="row">
+							<div class="col-xs-6">
+								Email ID  :<input type="Email" name="emailid" placeholder="abc@example.com" class="form-control">
+							</div>	
+				</div>
+				<div class="row">
+							<div class="col-xs-4">
+							Password  :<input type="password" name="pass" placeholder="Password" class="form-control">
+							</div>	
+				</div>
+				<div class="row">
+							<div class="col-xs-4">
+								Confirmed Password:<input type="password" name="cpass" placeholder="Type same Password again" class="form-control">	
+							</div>	
+				</div>
+				<div class="row">
+							<div class="col-xs-3">
+								City      :<input type="text" name="city" placeholder="Los Angeles" class="form-control">
+							</div>	
+				</div><br>
+
+				<label class="radio-inline">
+					<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">Male
+				</label>
+				<label class="radio-inline">
+					<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">Female
+				</label>
+
+			</div>
+
+		<button type="submit" class="btn btn-success">Register Here </button>
+	</form><br><br>
+
+<!-- Registeration ends here-->	
+</div>
+
 
  <?php require('footer.php'); ?>
--- testjs.php
+++ testjs.php
@@ -1,52 +1,56 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
 	<title>Test Js</title>
 	<link rel="stylesheet" type="text/css" href="">
 </head>
 <body id="body">
+-->
+
+<body id="body">
 
 <?php include('header.php'); ?>
 
-<center>
-	
-	<div class="header">
 
-		<h1 id="heading">Testing JS Scripts</h1> 
+<div class="main_body">
 
-		<p>With Hardik Patil<br> This is page only for testing... </p>
-	</div>
+	<h1 id="heading">Testing JS Scripts</h1> 
 
-<h2>Entry Form</h2>
-<form name="myform" action="#" method="POST" onsubmit="return validateForm()">
+	<p>With Hardik Patil<br> This is page only for testing... </p>
 
-	<h1 id="time"></h1><br> <!--Display date here -->
-	
-	<label>User Name:</label>
-	<input type="text" name="uname"><br><br>
-	<label>Password:</label>
-	<input type="password" name="pass"><br><br>
+	<h2>Entry Form</h2>
+	<form name="myform" action="#" method="POST" onsubmit="return validateForm()">
 
-	<label>Choose color to change Background</label>
-	<select name="background" id="background" onchange="changeBackground(this)">
-		<option value="#6B8E23">OLIVEDRAB</option>
-		<option value="#98FB98">PALEGREEN</option>
-		<option value="#2E8B57">SEAGREEN</option>
-		<option value="#008B8B">DARKCYAN</option>
-		<option value="#F4A460">SANDYBROWN</option>
-		<option value="#DAA520">GOLDENROD</option>
-		<option value="#FFD700">GOLD</option>		
-		<option value="#A9A9A9">DARKGRAY</option>
-		<option value="#4682B4">STEELBLUE</option>
-		<option value="#D2B48C">TAN</option>
-	</select><br><br>
+		<h1 id="time"></h1><br> <!--Display date here -->
+		
+		<label>User Name:</label>
+		<input type="text" name="uname"><br><br>
+		<label>Password:</label>
+		<input type="password" name="pass"><br><br>
 
-	<button onmouseover="showDate()">What Day is today ?</button><br><br>
+		<label>Choose color to change Background</label>
+		<select name="background" id="background" onchange="changeBackground(this)">
+			<option value="#6B8E23">OLIVEDRAB</option>
+			<option value="#98FB98">PALEGREEN</option>
+			<option value="#2E8B57">SEAGREEN</option>
+			<option value="#008B8B">DARKCYAN</option>
+			<option value="#F4A460">SANDYBROWN</option>
+			<option value="#DAA520">GOLDENROD</option>
+			<option value="#FFD700">GOLD</option>		
+			<option value="#A9A9A9">DARKGRAY</option>
+			<option value="#4682B4">STEELBLUE</option>
+			<option value="#D2B48C">TAN</option>
+		</select><br><br>
 
-	<input type="submit" value="Login">
-</form>
+		<button onmouseover="showDate()">What Day is today ?</button><br><br>
 
-</center>
+		<input type="submit" value="Login">
+	</form>	
+</div>
+
+
+
+
 
 <script type="text/javascript">
 
